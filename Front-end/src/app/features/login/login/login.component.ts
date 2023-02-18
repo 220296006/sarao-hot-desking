69d9a47ad3b3164dce1e7341963d3a0adf4c67e2
Thabiso Matsaba <you@example.com>
2023-02-18 11:19:35 +0200

    7nth commit

---
+++ Front-end/src/app/features/login/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  function () {
+    'use strict'
+    const forms = document.querySelectorAll('.requires-validation')
+    Array.from(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault()
+            event.stopPropagation()
+          form.classList.add('was-validated')
+        }, false)
+      })
+    }
+    
+}
